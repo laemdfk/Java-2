76314a7b453021f4eb35a5cb331ad96afe9112f5
laemdfk <kappa021618@icloud.com>
2022-07-26 01:41:48 +0900

    Add files via upload

---
+++ Java-Chapter3/Ready03.java
@@ -0,0 +1,13 @@
+// ↓ クラスブロックの始まり
+public class Ready03 {
+  // ↓ メソッドブロックの始まり (インデントしてわかりやすくする)
+  public static void main(String[] args) {
+    // ↓ コメントを追加 (メソッドブロックよりさらにインデントしてわかりやすくする)
+    // メソッドの処理を記述
+    System.out.println("Hello World!"); // ← 文字列を出力する処理
+    System.out.println("Hello Java!");
+    System.out.println("Hello WebCamp!");
+  }
+  // ↑ メソッドブロックの終わり
+}
+// ↑ クラスブロックの終わり
